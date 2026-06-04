5c9ced20919f82fb0b86719e1de8fde95e5604ab
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2026-06-04 15:23:34 -0400

    finalize Weber cohort curation

--- scripts/format_downloaded_data.R
+++ scripts/format_downloaded_data.R
@@ -10,22 +10,14 @@ work_dir <- "data"
 # load clinical metadata from https://precog.stanford.edu/
 clin <- readRDS(file.path('C:/Insight/data/PRECOG', 'ici_precog_all_datasets_clinical.rds'))
 datasetID <-  "CheckMate_064"
-
 clin <- clin[clin$Dataset == datasetID, ]
 
 write.table(clin, file=file.path(work_dir, 'CLIN.txt'), sep = "\t" , quote = FALSE , row.names = FALSE)
 
 # EXP_TPM.tsv
-clin <- readRDS(file.path('C:/Insight/data/PRECOG', 'ici_precog_all_datasets_expression.rda'))
-expr <- exprs(eset)
-dim(expr)
-
-## gene annotation
-annot <- readRDS(file.path('C:/Insight/data/PRECOG', 'ici_precog_dataset_annotations.rds'))
-feat <- fData(eset)
-head(feat)
-
-genes <- feat$ORF
-rownames(expr) <- genes
+expr <- readRDS(file.path('C:/Insight/data/PRECOG', 'ici_precog_all_datasets_expression.rds'))
+expr <- expr[grepl("_rna_064$", rownames(expr)), ]
+expr <- expr[, colSums(!is.na(expr)) > 0]
+expr <- t(expr)
 
 write.table(expr, file=file.path(work_dir, 'EXP_TPM.tsv'), sep = "\t" , quote = FALSE , row.names = TRUE, col.names=TRUE)
